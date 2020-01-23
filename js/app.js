eb6cbb554cd41fc0fde35e8e7d8a913918ff06b9
eilonosher <54228798+eilonosher@users.noreply.github.com>
2020-01-23 17:56:39 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,141 @@
+var teacherMap = new Map();
+var startTime;
+var endTime;
+var user;
+var students;
+var teachers;
+$(document).ready(function () {
+    console.log("ready!");
+    user = localStorage.getItem('userName');
+    initDataBase();
+
+
+});
+
+function hideLoading() {
+    var x = document.getElementById("loading");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function writeTheHourToHtmlPage() {
+    for (let hour = this.startTime; hour < this.endTime; hour++) {
+        document.getElementById("table").innerHTML += "<tr id='" + hour + "00'" + "></th>";
+        document.getElementById("table").innerHTML += "<tr id='" + hour + "20'" + "></th>";
+        document.getElementById("table").innerHTML += "<tr id='" + hour + "40'" + "></th>";
+    }
+
+
+}
+function addTheHoursCol() {
+    document.getElementById("teacher").innerHTML += "<th></th>";
+    for (let hour = this.startTime; hour < this.endTime; hour++) {
+        document.getElementById(hour + "00").innerHTML += "<td>" + hour + "00" + "</td>";
+        document.getElementById(hour + "20").innerHTML += "<td>" + hour + "20" + "</td>";
+        document.getElementById(hour + "40").innerHTML += "<td>" + hour + "40" + "</td>";
+    }
+
+
+}
+
+
+function buildTable() {
+    for (var key in teachers) {
+        let teacherName = teachers[key].name;
+        let teacherMeeting = teachers[key].list;
+
+        teacherMap.set(teacherName, new Teacher(teacherName, startTime, endTime, teacherMeeting));
+    }
+}
+
+function handleEvnet(id) {
+    var selectedName = id.split("-")[0];
+    var selectedTime = id.split("-")[1];
+    let teacher = teacherMap.get(selectedName);
+   let checkingIfAnotherHourMark =  teacher.checkingIfAnotherHourMark(user);
+    if(checkingIfAnotherHourMark){
+        teacher.userChangeHour(user,selectedTime);
+        deleteFromDB(selectedName,user,teacher,selectedTime)
+    }
+    else{
+        updateDataBase(selectedName,selectedTime)
+    }
+    let check = teacher.updateCell(user,selectedTime,"green");
+    //read
+    
+    //set
+    
+    // console.log(commentsRef)
+}
+
+function updateDataBase(selectedName,selectedTime){
+    var rootRef = firebase.database().ref();
+    var storesRef = rootRef.child('/teacher/' + selectedName + '/list');
+    var newStoreRef = storesRef.push();
+
+    newStoreRef.set({
+        time: selectedTime,
+        name: user
+
+    });
+}
+
+function deleteFromDB(selectedName,user,teacher,selectedTime){
+    firebase.database().ref('/teacher/' + selectedName + '/list').once('value').then(function (snapshot) {
+        let keys = [];
+        snapshot.forEach(function (item) {
+            if(item.val().name === user)
+                deleteNode(item.key,selectedName,user);
+            keys.push(item.key);
+        });
+         //let check = teacher.updateCell(user,selectedTime,"green");
+    //read
+    
+    //set
+         updateDataBase(selectedName,selectedTime)
+        
+
+    });
+}
+function deleteNode(key,selectedName,user){
+    firebase.database().ref('/teacher/' + selectedName + '/list/' + key).remove();
+    
+}
+function initEvnet() {
+    $('#table tr td').click(function () {
+        let mark = handleEvnet($(this).attr('id'));
+        console.log(mark)
+    });
+}
+
+function initDataBase() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyAisF9DxbrZJv2yB0TkLjFTephoNWlY9-U",
+        authDomain: "scheduleproject-f7ca1.firebaseapp.com",
+        databaseURL: "https://scheduleproject-f7ca1.firebaseio.com",
+        projectId: "scheduleproject-f7ca1",
+        storageBucket: "scheduleproject-f7ca1.appspot.com",
+        messagingSenderId: "886389423020",
+        appId: "1:886389423020:web:1510d86fa70d38cfe5d809",
+        measurementId: "G-40Z048RQ98"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    // Initialize Firebase
+    firebase.database().ref('//').once('value').then(function (snapshot) {
+        students = snapshot.val().student;
+        startTime = snapshot.val().hour.start;
+        endTime = snapshot.val().hour.end;
+        teachers = snapshot.val().teacher;
+        writeTheHourToHtmlPage();
+        addTheHoursCol();
+        buildTable();
+        initEvnet();
+        hideLoading();
+    });
+
+
+}
